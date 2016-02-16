d0f1ecf70c84efcf5650a261299776aa68e64512
Viacheslav <a05@ukr.net>
2016-02-16 21:11:11 +0200

    Quality switch updated

--- js/buttonQuality.js
+++ js/buttonQuality.js
@@ -4,19 +4,23 @@ var $btnQuality = window.viewerState.$btnQuality,
     $svgQuality = document.querySelector('.btn_quality__icon'),
     $video = window.viewerState.$video,
     $source = window.viewerState.$source,
+    $btnMenuOn = window.viewerState.$btnMenuOn,
     classList = window.viewerState.classList,
     link = ''
 
 styleQualityButton()
 
-$btnQuality.addEventListener('click', function(){
+$btnQuality.addEventListener('click', toggleQuality)
+document.addEventListener("fullscreenchange", exitFullScreen)
+document.addEventListener("webkitfullscreenchange", exitFullScreen)
+document.addEventListener("mozfullscreenchange", exitFullScreen)
+document.addEventListener("MSFullscreenChange", exitFullScreen)
+$btnMenuOn.addEventListener('click', lowerQuality)
+
+function toggleQuality(){
     if (window.viewerState.active$input) {
         if (window.viewerState.highQuality) {
-            window.viewerState.highQuality = false
-            link = window.viewerState.active$input.getAttribute('data-link-lq')
-            $video.setAttribute('src', link)
-            $source.setAttribute('src', link)
-            $video.play()
+            lowerQuality()
         } else {
             window.viewerState.highQuality = true
             link = window.viewerState.active$input.getAttribute('data-link-hq')
@@ -26,13 +30,19 @@ $btnQuality.addEventListener('click', function(){
         }
         styleQualityButton()
     }
-})
-
-document.addEventListener("fullscreenchange", styleQualityButton)
-document.addEventListener("webkitfullscreenchange", styleQualityButton)
-document.addEventListener("mozfullscreenchange", styleQualityButton)
-document.addEventListener("MSFullscreenChange", styleQualityButton)
-
+}
+function exitFullScreen() {
+    if(!window.viewerState.ask$boxInFullScreen()){
+        lowerQuality()
+    }
+}
+function lowerQuality() {
+    window.viewerState.highQuality = false
+    link = window.viewerState.active$input.getAttribute('data-link-lq')
+    $video.setAttribute('src', link)
+    $source.setAttribute('src', link)
+    $video.play()
+}
 function styleQualityButton() {
     if (window.viewerState.highQuality) {
         classList.remove($svgQuality, 'disabled')
